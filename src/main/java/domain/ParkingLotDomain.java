3fdfb541c37f641805f01d974ded3d0e3ad4e738
sonorks <julivas96@gmail.com>
2018-05-09 16:18:24 -0500

    error stubbing2

--- src/main/java/domain/ParkingLotDomain.java
+++ src/main/java/domain/ParkingLotDomain.java
@@ -1,10 +1,5 @@
 package domain;
 
-import org.mockito.stubbing.OngoingStubbing;
-import org.springframework.stereotype.Repository;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
 
 public class ParkingLotDomain {
 
